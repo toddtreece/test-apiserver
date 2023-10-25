a12c977898a87fa239d0a7cf942bc8bd497bad2a
Todd Treece <toddtreece@gmail.com>
2023-10-24 22:04:49 -0400

    cleanup

--- pkg/registry/example/example/resource.go
+++ pkg/registry/example/example/resource.go
@@ -11,38 +11,29 @@ import (
 	"k8s.io/apiserver/pkg/registry/rest"
 )
 
-// ResourceCallREST implements the proxy subresource for a Pod
 type ResourceCallREST struct {
 	Store *genericregistry.Store
 }
 
-// Implement Connecter
 var _ = rest.Connecter(&ResourceCallREST{})
 
-var proxyMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+var methods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 
-// New returns an empty podProxyOptions object.
 func (r *ResourceCallREST) New() runtime.Object {
 	return &example.ResourceCallOptions{}
 }
 
-// Destroy cleans up resources on shutdown.
 func (r *ResourceCallREST) Destroy() {
-	// Given that underlying store is shared with REST,
-	// we don't destroy it here explicitly.
 }
 
-// ConnectMethods returns the list of HTTP methods that can be proxied
 func (r *ResourceCallREST) ConnectMethods() []string {
-	return proxyMethods
+	return methods
 }
 
-// NewConnectOptions returns versioned resource that represents proxy parameters
 func (r *ResourceCallREST) NewConnectOptions() (runtime.Object, bool, string) {
 	return &example.ResourceCallOptions{}, true, "path"
 }
 
-// Connect returns a handler for the pod proxy
 func (r *ResourceCallREST) Connect(ctx context.Context, id string, opts runtime.Object, responder rest.Responder) (http.Handler, error) {
 	resourceCallOpts, ok := opts.(*example.ResourceCallOptions)
 	if !ok {
@@ -57,5 +48,5 @@ type fakeHandler struct {
 
 func (f *fakeHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(req.URL.Path))
+	w.Write([]byte("Path:\n" + req.URL.Path + "\n"))
 }
